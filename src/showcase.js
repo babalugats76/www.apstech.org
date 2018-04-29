dfec1fcf154f052280506e634425d58edb2699dc
James Colestock <james@colestock.com>
2018-04-29 17:02:13 -0600

    Update showcase.js

--- src/showcase.js
+++ src/showcase.js
@@ -10,49 +10,49 @@ function GameListController($scope) {
             name: 'Jason',
             title: 'Half-Blood',
             path: 'half-blood',
-            avatar: 'jasons-avatar-WxH.png',
+            avatar: 'jasons-avatar-398x398.png',
             tagline: 'Half angel, half demon.'
         },
         {
             name: 'Ivan',
             title: 'Bezerk',
             path: 'bezerk',
-            avatar: 'ivanj-avatar-WxH.png',
+            avatar: 'ivanj-avatar-398x398.png',
             tagline: 'Fight for your honor.'
         },
         {
             name: 'Elesvan',
             title: 'Save the Bananas',
             path: 'save-the-bananas',
-            avatar: 'elesvanp-avatar-WxH.png',
+            avatar: 'elesvanp-avatar-597x597.jpg',
             tagline: 'Help Little Wu save the bananas.'
         },
         {
             name: 'Caleb',
             title: 'We\'re Not Dead',
             path: 'were-not-dead',
-            avatar: 'calebt-avatar-WxH.png',
+            avatar: 'calebt-avatar-398x398.png',
             tagline: 'What if zombies were not dead in the first place.'
         },
         {
             name: 'Alejandro',
             title: 'Monopoly Massacre',
             path: 'monopoly-massacre',
-            avatar: 'alejandrog-avatar-WxH.png',
+            avatar: 'alejandrog-avatar-398x398.png',
             tagline: 'Help Elena return the moon to fantastic times.'
         },
         {
             name: 'Lachlan',
             title: 'Escape the Storm',
             path: 'escape-the-storm',
-            avatar: 'lachlanp-avatar-WxH.png',
+            avatar: 'lachlanp-avatar-597x597.png',
             tagline: 'Escape the storm to stay alive.'
         },
         {
             name: 'Tinn',
             title: 'To Return Home',
             path: 'to-return-home',
-            avatar: 'tinnp-avatar-WxH.png',
+            avatar: 'tinnp-avatar-398x398.png',
             tagline: 'Guide Captain K on his journey home.'
         }
   ];
