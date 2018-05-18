8e5f0bb618ff99b18d73bc2802631eff2376a482
James Colestock <james@colestock.com>
2018-05-18 10:16:05 -0600

    Updated showcase JSON and index

--- dist/www-apstech-org.js
+++ dist/www-apstech-org.js
@@ -16,53 +16,25 @@ angular.module('showcaseApp').controller('GameListController', GameListControlle
 function GameListController($scope) {
     $scope.games = [
         {
-            name: 'Jason',
-            title: 'Half-Blood',
-            path: 'half-blood',
-            avatar: 'jasons-avatar-398x398.png',
-            tagline: 'Half angel, half demon.'
-        },
-        {
             name: 'Ivan',
             title: 'Bezerk',
             path: 'bezerk',
             avatar: 'ivanj-avatar-398x398.png',
-            tagline: 'Fight for your honor.'
-        },
-        {
-            name: 'Elesvan',
-            title: 'Save the Bananas',
-            path: 'save-the-bananas',
-            avatar: 'elesvanp-avatar-597x597.jpg',
-            tagline: 'Help Little Wu save the bananas.'
+            tagline: 'Fight for your honor'
         },
         {
             name: 'Caleb',
-            title: 'We\'re Not Dead',
-            path: 'were-not-dead',
+            title: 'Millennium Ship',
+            path: 'millennium-ship',
             avatar: 'calebt-avatar-398x398.png',
-            tagline: 'What if zombies were not dead in the first place.'
-        },
-        {
-            name: 'Alejandro',
-            title: 'Monopoly Massacre',
-            path: 'monopoly-massacre',
-            avatar: 'alejandrog-avatar-398x398.png',
-            tagline: 'Help Elena return the moon to fantastic times.'
-        },
-        {
-            name: 'Lachlan',
-            title: 'Escape the Storm',
-            path: 'escape-the-storm',
-            avatar: 'lachlanp-avatar-597x597.png',
-            tagline: 'Escape the storm to stay alive.'
+            tagline: 'Fly or die...'
         },
         {
             name: 'Tinn',
             title: 'To Return Home',
             path: 'to-return-home',
             avatar: 'tinnp-avatar-398x398.png',
-            tagline: 'Guide Captain K on his journey home.'
+            tagline: 'Guide Captain K on his journey home'
         }
   ];
 }
--- src/showcase.js
+++ src/showcase.js
@@ -7,25 +7,11 @@ angular.module('showcaseApp').controller('GameListController', GameListControlle
 function GameListController($scope) {
     $scope.games = [
         {
-            name: 'Jason',
-            title: 'Half-Blood',
-            path: 'half-blood',
-            avatar: 'jasons-avatar-398x398.png',
-            tagline: 'Half angel, half demon.'
-        },
-        {
             name: 'Ivan',
             title: 'Bezerk',
             path: 'bezerk',
             avatar: 'ivanj-avatar-398x398.png',
-            tagline: 'Fight for your honor.'
-        },
-        {
-            name: 'Elesvan',
-            title: 'Save the Bananas',
-            path: 'save-the-bananas',
-            avatar: 'elesvanp-avatar-597x597.jpg',
-            tagline: 'Help Little Wu save the bananas.'
+            tagline: 'Fight for your honor'
         },
         {
             name: 'Caleb',
@@ -35,25 +21,11 @@ function GameListController($scope) {
             tagline: 'Fly or die...'
         },
         {
-            name: 'Alejandro',
-            title: 'Monopoly Massacre',
-            path: 'monopoly-massacre',
-            avatar: 'alejandrog-avatar-398x398.png',
-            tagline: 'Help Elena return the moon to fantastic times.'
-        },
-        {
-            name: 'Lachlan',
-            title: 'Escape the Storm',
-            path: 'escape-the-storm',
-            avatar: 'lachlanp-avatar-597x597.png',
-            tagline: 'Escape the storm to stay alive.'
-        },
-        {
             name: 'Tinn',
             title: 'To Return Home',
             path: 'to-return-home',
             avatar: 'tinnp-avatar-398x398.png',
-            tagline: 'Guide Captain K on his journey home.'
+            tagline: 'Guide Captain K on his journey home'
         }
   ];
 }
